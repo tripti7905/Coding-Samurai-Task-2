6cdac705eadad8f09ad6512c3c9c5d6f619ac898
Tripti Tiwari <119399826+tripti7905@users.noreply.github.com>
2023-09-21 12:01:20 +0530

    Add files via upload

---
+++ calculator java/Simple-Java-Calculator/src/simplejavacalculator/SimpleJavaCalculator.java
@@ -0,0 +1,15 @@
+
+package simplejavacalculator;
+
+public class SimpleJavaCalculator {
+
+   public static void main(String[] args) {
+      try {
+         UI uiCal = new UI();
+         uiCal.init();
+      } catch (Exception e) {
+         System.out.println(e.getMessage());
+      }
+
+   }
+}
